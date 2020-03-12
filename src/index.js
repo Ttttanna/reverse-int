fc9aff4f5876c32fb4a8998f268fbf767961fc0e
Tatsiana Alsheuskaya <ttttanna@gmail.com>
2020-03-12 21:05:50 +0300

    fix: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+  let arr = String(n).split("");
+  let answer = [];
+  for (let int of arr) {
+    if ( 0 <= (+int) && (+int) <= 9) {
+      answer.push(int);
+    }
+  }
+  if (answer[answer.length -1] === 0) {
+    answer.pop();
+  }
+  return answer.reverse().join("");
 }
